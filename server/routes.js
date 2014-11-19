4e3df4232cb84dd08044747e95216ceb07cd2f47
Mari Batilando <mari.batilando@gmail.com>
2014-11-19 11:27:08 -0800

    Remove unnecessary files

--- server/routes.js
+++ server/routes.js
@@ -11,8 +11,6 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/users', require('./api/user'));
   app.use('/api/pages', require('./api/page'));
-  app.use('/api/things', require('./api/thing'));
-  
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
